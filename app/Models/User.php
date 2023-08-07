e59399a5f5ebf2091be678f40a963a83561dd57b
Bahdanovich <bahdanovich@internet.ru>
2023-08-07 15:36:28 +0300

    Added Role, User, Permission models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'role_id'
     ];
 
     /**
@@ -40,6 +42,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
