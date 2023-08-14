bc77ebcc5c355ee7ba2ff07b496396a374924c8f
Bahdanovich <bahdanovich@internet.ru>
2023-08-14 19:32:54 +0300

    Some changes

--- resources/views/inc/admin/navigation.blade.php
+++ resources/views/inc/admin/navigation.blade.php
@@ -1,7 +1,7 @@
 @if(Auth::user())
     <ul>
-        <li><a href="{{ route('roles.index') }}">Roles</a></li>
-        <li><a href="{{ route('permissions.index') }}">Permissions</a></li>
+{{--        <li><a href="{{ route('roles.index') }}">Roles</a></li>--}}
+{{--        <li><a href="{{ route('permissions.index') }}">Permissions</a></li>--}}
         <li><a href="#">Users</a></li>
         <li><a href="{{ route('tasks.index') }}">Tasks</a></li>
     </ul>
